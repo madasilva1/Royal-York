7a84a8e79c4a1cfec0755d4beffe43ec4fee83c3
madasilva1 <madasilva195@gmail.com>
2020-02-06 14:48:09 -0500

    RoomsActivity has been included linked floor to rooms

--- app/src/main/java/com/example/android/fristapp/FloorActivity.java
+++ app/src/main/java/com/example/android/fristapp/FloorActivity.java
@@ -16,7 +16,20 @@ public class FloorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor);
+// Find the View that shows the Rooms category
+        TextView rooms = (TextView) findViewById(R.id.rooms);
+        // Set a click listener on that View
+        rooms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //creat a new Intent to open the {@link roomsActivity}
 
+                Intent RoomsActivity = new Intent(FloorActivity.this, RoomsActivity.class);
+                startActivity(RoomsActivity);
+                // Toast.makeText(getApplicationContext(),"MainActivity java",Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
     }
 
---
+++ app/src/main/java/com/example/android/fristapp/RoomsActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.fristapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class RoomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rooms);
+
+
+    }
+}
