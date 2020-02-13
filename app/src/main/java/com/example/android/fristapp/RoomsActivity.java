a4548f33e414a8a5898e0783f3fe1d9fd4f495c7
madasilva1 <madasilva195@gmail.com>
2020-02-13 12:38:52 -0500

    I have returned to the same point were i was before

--- app/src/main/java/com/example/android/fristapp/RoomsActivity.java
+++ app/src/main/java/com/example/android/fristapp/RoomsActivity.java
@@ -15,24 +15,12 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class RoomsActivity extends AppCompatActivity {
-    private TextView editTxt;
 
-    private ListView list;
-    private ArrayAdapter<Integer> adapter;
-    private ArrayList<Integer> arrayList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rooms);
-        list = (ListView) findViewById(R.id.List_of_rooms);
-        arrayList = new ArrayList<Integer>();
-        // Adapter: You need three parameters 'the context, id of the layout (it will be where the data is shown),
-        // and the array that contains the data
-        adapter = new ArrayAdapter<Integer>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
-        // Here, you set the data in your ListView
-        list.setAdapter(adapter);
-        // Set a click listener on that View
-      arrayList.add(0,20);
-      arrayList.add(1,23);
+
     }
 }
